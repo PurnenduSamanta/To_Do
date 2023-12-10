0cc35e27582631912265f4dc583fb73972a92fd0
PurnenduSamanta <joysamanta123@gmail.com>
2023-12-10 11:47:04 +0530

    4th_10_12_2023

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo/screen/add_task.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,10 +11,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String uid = 'empty';
+  @override
+  void initState() {
+    getUID();
+    super.initState();
+  }
+
+  getUID() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = auth.currentUser!;
+    setState(() {
+      uid = user.uid;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(title: Text("ToDo"),backgroundColor: Colors.blueAccent,),
+      appBar: AppBar(
+        title: Text("ToDo"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('tasks')
+                .doc(uid)
+                .collection('myTasks')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                final docs = snapshot.data?.docs;
+                return ListView.builder(
+                  itemCount: docs?.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          color: Colors.lightGreenAccent,
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsetsDirectional.all(10),
+                      height: 90,
+                      child: Column(
+                        children: [Text(docs?[index]['title'])],
+                      ),
+                    );
+                  },
+                );
+              }
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddTask()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 30,
+        ),
+      ),
     );
   }
 }
---
+++ lib/screen/add_task.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AddTask extends StatefulWidget {
+  const AddTask({super.key});
+
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  addTaskToFirebas() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = await auth.currentUser!;
+    String uid = user.uid;
+    var time = DateTime.now();
+    await FirebaseFirestore.instance
+        .collection('tasks')
+        .doc(uid)
+        .collection('myTasks')
+        .doc(time.toString())
+        .set({
+      'title': titleController.text,
+      'description': descriptionController.text,
+      'time': time.toString()
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Task'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: titleController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide()),
+                  labelText: 'title'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: descriptionController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide()),
+                  labelText: 'description'),
+            ),
+            SizedBox(height: 10),
+            Container(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      addTaskToFirebas();
+                    },
+                    child: Text('Add Task')))
+          ],
+        ),
+      ),
+    );
+  }
+}
